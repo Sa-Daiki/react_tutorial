431747b3c5ae80b4e8f8c140ae90650a8d27f4a2
Sa-Daiki <sato-daiki@evolu.tokyo>
2022-11-08 16:55:34 +0900

    ♻️ Square切り出し

--- src/component/Square/Square.tsx
+++ src/component/Square/Square.tsx
@@ -1,8 +1,10 @@
 import { SquareProps } from "./type";
 
-export const Square = ({ onClick, value }: SquareProps) => (
-  <button className="square" onClick={onClick}>
-    {value}
-  </button>
-);
+export const Square = ({ onClick, value }: SquareProps) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
 export default Square;
